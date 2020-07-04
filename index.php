10cf255f43f7d41fc3b3ae85be19dcf204342b2f
Wagner <wg.o.costa@gmail.com>
2020-07-03 23:09:28 -0300

    Pagina principal

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ include_once 'includes/header.php';
 ?>
 
 <div class="row">
-    <div class="col s12 m6 push-m3 red">
-      <table>
+    <div class="col s12 m6 push-m3">
+      <h3 class="light">Vendedores</h3>
+      <table class="striped">
         <thead>
           <tr>
             <th>Código:</th>
@@ -16,11 +17,20 @@ include_once 'includes/header.php';
         <tbody>
           <tr>
             <td>1</td>
-            <td>Wagner</td>
+            <td>Wagner Costa 007</td>
             <td>wg.o.costa@gmail.com</td>
+            <td><a href="" class="btn-floating blue">
+              <i class="material-icons">edit</i>
+            </a></td>
+            <td><a href="" class="btn-floating red">
+              <i class="material-icons">delete</i>
+            </a></td>
+           
           </tr>
         </tbody>
       </table>
+      <br>
+      <a href="" class="btn">Adicionar Vendedor</a>
     </div>
 </div>
 
